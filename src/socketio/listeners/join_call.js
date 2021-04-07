dc480ef9ead61ce72c093f23e904251981b3213b
Ngoctrong102 <trong.vo102@hcmut.edu.vn>
2021-04-07 21:43:36 +0700

    xong video, đẩy lên test heroku

--- src/socketio/listeners/join_call.js
+++ src/socketio/listeners/join_call.js
@@ -3,7 +3,7 @@ var { join_call } = require('../events');
 module.exports = socket => {
     socket.on("JOIN_CALL", async({ conversationID, peerID }) => {
         var conversation = await ConversationService.getConversationById(conversationID);
-        if (conversation.users.includes(socket.user._id)) {
+        if (conversation.users.map(u => u._id).includes(socket.user._id)) {
             socket.join(conversation._id);
             join_call(socket, { conversationID, peerID })
         }
